9078c75c89ae948ffcfbc6459a775108ac16290e
John Irungu <153911261+Chege-John@users.noreply.github.com>
2024-07-30 23:25:34 +0300

    sentry.server.config.ts

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -5,7 +5,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://7b883d075caa19b41fd9b00ae313a1c6@o4506813739368448.ingest.us.sentry.io/4507222371729408",
+  dsn: "https://f8ff100b959ce1ae576106ef0dfd02ec@o4507692546588672.ingest.us.sentry.io/4507692558909440",
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
